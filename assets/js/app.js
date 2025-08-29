13d5594d72e321b4f46164ed46a0606f2d5fb9cb
Md. Asif <acacdmml@gmail.com>
2025-08-30 01:10:32 +0600

    commit - 4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,33 @@
-let mobileSearch = document.querySelector('.mobile_search');
-let searchBody = document.querySelector('#mobile_search');
+$(function () {
+    let mobileSearch = document.querySelector('.mobile_search');
+    let searchBody = document.querySelector('#mobile_search');
+    
+    let searchClose = document.querySelector('.close');
+    
+    
+    mobileSearch.addEventListener('click', function() {
+        searchBody.classList.add('mobile_search_active');
+    
+    });
+    
+    searchClose.addEventListener('click', function() {
+        searchBody.classList.remove('mobile_search_active');
+    });
 
-let searchClose = document.querySelector('.close');
-
-
-mobileSearch.addEventListener('click', function() {
-    searchBody.classList.add('mobile_search_active');
-
+    // banner slider
+    $('.parent_slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        nextArrow: '<span class="right"><iconify-icon icon="prime:arrow-right" width="24" height="24"></iconify-icon></span>',
+        prevArrow: '<span class="left"><iconify-icon icon="prime:arrow-left" width="24" height="24"></iconify-icon></span>',
+    });
 });
 
-searchClose.addEventListener('click', function() {
-    searchBody.classList.remove('mobile_search_active');
-});
+
+
+
+
+
+
+
